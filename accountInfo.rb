15f6157aabe5e358e0d3ef0ebab9f22c71ccc07c
Jon O'Brien <jvo7822@rit.edu>
2016-01-24 01:24:31 -0500

    refactored loop into function for games

--- accountInfo.rb
+++ accountInfo.rb
@@ -12,11 +12,15 @@ require 'json'
 require 'steam_web_api'
 
 apikey = 'steamdev-api-key-here'  # steam app api key
+apikey = 'A9F3DD5EBFB794FC3C87FF96FAD2F423'
 pkey = '76561198017058828'        # steamid of a user
+ownedAppIDs = []
 steamVanityNames = []
 steamIdKeys = ''
 
-# https://github.com/lostisland/faraday
+
+# Faraday undefined -> check api keys
+#### https://github.com/lostisland/faraday
 conn = Faraday.new(:url => 'http://api.steampowered.com/') do |faraday|
   faraday.request  :url_encoded                          # form-encode POST params
   #faraday.response :logger                              # log requests to STDOUT
@@ -25,18 +29,22 @@ conn = Faraday.new(:url => 'http://api.steampowered.com/') do |faraday|
 end
 
 
-# for Rails, you can put this code in initializer: config/initializers/steam_web_api.rb
+# for Rails, you can put this code in initializer: 
+# config/initializers/steam_web_api.rb
 SteamWebApi.configure do |config|
     config.api_key = apikey
 end
 
+# get the same games a user has
+# with the original user
+def getSharedGames(plyr, uname)
+	puts('global games: ')
+	puts(plyr.owned_games)
+	puts()
+	puts('games for: ' + uname)
+end
 
-
-
-
-
-# query for the chose user's info
-
+# query for the chose user's info (based on community id)
 
 player = SteamWebApi::Player.new(pkey)
 data = player.owned_games
@@ -44,51 +52,44 @@ data = player.owned_games
 data = player.owned_games(include_played_free_games: true, include_appinfo: true)
 puts('username: ' + player.summary.profile['personaname'])
 
-
-# puts '======================='
-# data.games.each { |game|
-# 	p game['name']
-# }
-# puts '======================='
-
-
-## if <include_appinfo> is set to true in the method call
-## you also get information on images and stats of the games
-#
-# game = data.games.first
-# puts '                         name: ' + game['name']
-# puts 'time played over last 2 weeks: ' + game['playtime_2weeks'].inspect             # total number of minutes played in the last 2 weeks
-# puts '          image icon filename: ' + game['img_icon_url']                        # filename of game's icon
-# puts '          image logo filename: ' + game['img_logo_url']                        # filename of game logo
-# puts '              game has stats?: ' + game['has_community_visible_stats'].inspect # indicates there is a stats page with achievements or other game stats available for this game
-
-
-
-
-
 # take the string of all steamids from friends list
 # query the steam api for their display names
-player.friends.friends.each { |friend|  
+player.friends.friends.each { |friend| 
 	steamIdKeys += friend['steamid'] + ','
 }
 
-response = conn.get 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + apikey + '&steamids=' + steamIdKeys
 
+# hash of owned game data with array of games
+# separate out the app IDs of games owned for usage
+player.owned_games.games.each { |game|
+	ownedAppIDs.push(game['appid'])
+}
+puts()
+puts('sorted owned')
+puts(ownedAppIDs)
+puts('done owned')
+
+response = conn.get 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + apikey + '&steamids=' + steamIdKeys
 JSON.parse(response.body)['response']['players'].each { |friend|
 	steamVanityNames.push(friend['personaname'])
 }
 
+
+
+
+
+
+
+
 # visually confirm the two counts are the same
-p('      nSteamid len: ' + player.friends.friends.count.to_s)
-p('nDisplay names len: ' + steamVanityNames.count.to_s)
-
-
-
+puts('      nSteamid len: ' + player.friends.friends.count.to_s)
+puts('nDisplay names len: ' + steamVanityNames.count.to_s)
 puts
 puts 'done'
 
 
 
+getSharedGames(player, "chris")
 
 
 
