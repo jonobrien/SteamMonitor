405d15bd02d1bbbc903710fd2132d960c14d7c96
Jon O'Brien <jvo7822@rit.edu>
2017-07-19 00:26:43 -0400

    multiplayer identification support, ideas, tabs to spaces

--- accountInfo.rb
+++ accountInfo.rb
@@ -10,60 +10,60 @@ load 'user.rb'
 
 def main
 
-	apikey = 'steamdev-api-key-here'  # steam app api key
-	pID    = '76561198017058828'        # steamid of a user
+    apikey = 'steamdev-api-key-here'  # steam app api key
+    pID    = '76561198017058828'        # steamid of a user
 
 
-	# Faraday undefined -> check api keys
-	#### https://github.com/lostisland/faraday
-	$conn = Faraday.new(:url => 'http://api.steampowered.com/') do |faraday|
-	  faraday.request  :url_encoded                          # form-encode POST params
-	  #faraday.response :logger                              # log requests to STDOUT
-	  faraday.headers['Content-Type'] = 'application/json'   # set the content type to JSON
-	  faraday.adapter  Faraday.default_adapter               # make requests with Net::HTTP
-	end
+    # Faraday undefined -> check api keys
+    #### https://github.com/lostisland/faraday
+    $conn = Faraday.new(:url => 'http://api.steampowered.com/') do |faraday|
+      faraday.request  :url_encoded                          # form-encode POST params
+      #faraday.response :logger                              # log requests to STDOUT
+      faraday.headers['Content-Type'] = 'application/json'   # set the content type to JSON
+      faraday.adapter  Faraday.default_adapter               # make requests with Net::HTTP
+    end
 
 
-	# for Rails, you can put this code in initializer: 
-	# config/initializers/steam_web_api.rb
-	SteamWebApi.configure do |config|
-	    config.api_key = apikey
-	end
-
-
-
-
-	# query for the chosen user's info (based on community id)
-	usr = User.new()
-	me = usr.getUser(pID)
-	myFriendStr = usr.getFriendIDstr(me)
-	myFriendIDs = myFriendStr.split(',')
-	myFriends = usr.getFriendPersonas(apikey, pID, myFriendStr)
-	puts('username: ' + me.summary.profile['personaname'])
-	# visually confirm the two counts are the same, id and persona
-	puts('      nSteamid friends len: ' + me.friends.friends.size.to_s)
-	puts('nDisplay names friends len: ' + myFriends.size.to_s)
-	puts('done converting friends list')
-	## get the games that three chosen users have in common
-
-	# my games and another, TODO stdin list
-	sharedGames1n2 = usr.getSharedGames(pID, myFriendIDs[1])##myFriendIDs)
-	# get P3 games
-	thirdPlayerGames = usr.getAllOwned(usr.getUser(myFriendIDs[2]))
-	# compare 1,2,3 games
-	sharedThird = usr.compareGames(sharedGames1n2, thirdPlayerGames)
-	puts(sharedThird)
-	# compare just first and third
-	# orig = usr.compareGames(usr.getAllOwned(usr.getUser(pID)), sharedThird)
+    # for Rails, you can put this code in initializer: 
+    # config/initializers/steam_web_api.rb
+    SteamWebApi.configure do |config|
+        config.api_key = apikey
+    end
 
 
 
 
+    # query for the chosen user's info (based on community id)
+    usr = User.new()
+    me = usr.getUser(pID)
+    myFriendStr = usr.getFriendIDstr(me)
+    myFriendIDs = myFriendStr.split(',')
+    myFriends = usr.getFriendPersonas(apikey, pID, myFriendStr)
+    puts('username: ' + me.summary.profile['personaname'])
+    # visually confirm the two counts are the same, id and persona
+    puts('      nSteamid friends len: ' + me.friends.friends.size.to_s)
+    puts('nDisplay names friends len: ' + myFriends.size.to_s)
+    puts('done converting friends list')
+    ## get the games that three chosen users have in common
 
-	# use dynamic player summary method
-	# https://github.com/sashiba/steam-web-api#get-accounts-summaries-for-list-of-players
-	puts()
-	usr.getAllFriendData(me)
+    # my games and another, TODO stdin list
+    sharedGames1n2 = usr.getSharedGames(pID, myFriendIDs[1])##myFriendIDs)
+    # get P3 games
+    thirdPlayerGames = usr.getAllOwned(usr.getUser(myFriendIDs[2]))
+    # compare 1,2,3 games
+    sharedThird = usr.compareGames(sharedGames1n2, thirdPlayerGames)
+    puts(sharedThird)
+    # compare just first and third
+    # orig = usr.compareGames(usr.getAllOwned(usr.getUser(pID)), sharedThird)
+
+
+
+
+
+    # use dynamic player summary method
+    # https://github.com/sashiba/steam-web-api#get-accounts-summaries-for-list-of-players
+    puts()
+    usr.getAllFriendData(me)
 
 
 
@@ -89,18 +89,18 @@ main
 
 -- the methods available to a player object  --
 
-[:steam_id, :steam_id=, :owned_games, :stats_for_game, :achievements, :summary, 
-	:friends, :recently_played_games, :playing_shared_game, :bans, :response, 
-	:response=, :get, :parse_response, :build_response, :to_json, :nil?, :===, 
-	:=~, :!~, :eql?, :hash, :<=>, :class, :singleton_class, :clone, :dup, :itself, 
-	:taint, :tainted?, :untaint, :untrust, :untrusted?, :trust, :freeze, :frozen?, 
-	:to_s, :inspect, :methods, :singleton_methods, :protected_methods, 
-	:private_methods, :public_methods, :instance_variables, :instance_variable_get, 
-	:instance_variable_set, :instance_variable_defined?, :remove_instance_variable, 
-	:instance_of?, :kind_of?, :is_a?, :tap, :send, :public_send, :respond_to?, 
-	:extend, :display, :method, :public_method, :singleton_method, 
-	:define_singleton_method, :object_id, :to_enum, :enum_for, :==, :equal?, :!, 
-	:!=, :instance_eval, :instance_exec, :__send__, :__id__]
+[:steam_id, :steam_id=, :owned_games, :stats_for_game, :achievements, :summary,
+    :friends, :recently_played_games, :playing_shared_game, :bans, :response,
+    :response=, :get, :parse_response, :build_response, :to_json, :nil?, :===,
+    :=~, :!~, :eql?, :hash, :<=>, :class, :singleton_class, :clone, :dup, :itself,
+    :taint, :tainted?, :untaint, :untrust, :untrusted?, :trust, :freeze, :frozen?,
+    :to_s, :inspect, :methods, :singleton_methods, :protected_methods,
+    :private_methods, :public_methods, :instance_variables, :instance_variable_get,
+    :instance_variable_set, :instance_variable_defined?, :remove_instance_variable,
+    :instance_of?, :kind_of?, :is_a?, :tap, :send, :public_send, :respond_to?,
+    :extend, :display, :method, :public_method, :singleton_method,
+    :define_singleton_method, :object_id, :to_enum, :enum_for, :==, :equal?, :!,
+    :!=, :instance_eval, :instance_exec, :__send__, :__id__]
 
 
 =end
--- user.rb
+++ user.rb
@@ -7,160 +7,217 @@ require 'steam_web_api'
 
 class User
 
-	### TODO -- make initializer with @vars and refactor
+    ### TODO -- make initializer with @vars and refactor
 
-	## take in the users community id and return their info
-	# def initialize(id)
-	# 	puts ('user initialized: ' + id)
-	# 	@user = SteamWebApi::Player.new(id)
-	# 	puts('queried data for: ' + @user.summary.profile['personaname'])
-	# 	return @user
-	# end
-	def initialize()
-		puts('[I] new player initialized')
-	end
-
-
-	# get the users steam information
-	# returns: steamwebapi player object
-	def getUser(pid)
-		puts('[I] accessing user info for: ' + pid.to_s)
-		# this seems computationaly heavy
-		@player = SteamWebApi::Player.new(pid)
-		puts('[I] retrieved user info for: ' + @player.summary.profile['personaname'])
-		return @player
-	end
+    ## take in the users community id and return their info
+    # def initialize(id)
+    #   puts ('user initialized: ' + id)
+    #   @user = SteamWebApi::Player.new(id)
+    #   puts('queried data for: ' + @user.summary.profile['personaname'])
+    #   return @user
+    # end
+    def initialize()
+        puts('[I] new player initialized')
+    end
 
 
-	# take the string of all steamids from friends list
-	# make csv string for conversion with getFriendVanities()
-	# returns: csv string of friend IDs
-	def getFriendIDstr(user)
-		@friendStr = ''
-		puts('[I] getting friend IDs for: ' + user.summary.profile['personaname'])
-		user.friends.friends.each { |friend| 
-			@friendStr += friend['steamid'] + ','
-		}
-		return @friendStr
-	end
+    # get the users steam information
+    # returns: steamwebapi player object
+    def getUser(pid)
+        puts('[I] accessing user info for: ' + pid.to_s)
+        # this seems computationaly heavy
+        @player = SteamWebApi::Player.new(pid)
+        puts('[I] retrieved user info for: ' + @player.summary.profile['personaname'])
+        return @player
+    end
 
 
-	# found a way to get from steamid to display names (the gem's methods were odd/slow)
-
-	# take in the users community id and convert the friend list
-	# from IDs to usernames as seen by the user normally
-	# returns: array of user personas as displayed in friends list
-	def getFriendPersonas(apikey, id, steamIdKeys)
-		@personas = []
-		response = $conn.get 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + apikey + '&steamids=' + steamIdKeys
-		JSON.parse(response.body)['response']['players'].each { |friend|
-			@personas.push(friend['personaname'])
-		}
-		return @personas
-	end
+    # take the string of all steamids from friends list
+    # make csv string for conversion with getFriendVanities()
+    # returns: csv string of friend IDs
+    def getFriendIDstr(user)
+        @friendStr = ''
+        puts('[I] getting friend IDs for: ' + user.summary.profile['personaname'])
+        user.friends.friends.each { |friend| 
+            @friendStr += friend['steamid'] + ','
+        }
+        return @friendStr
+    end
 
 
-	# hash of owned game data with array of games
-	# separate out the app IDs of games owned for usage
-	# returns: array of games user owns/f2p played
-	def getAllOwned(user)
-		@owned = Hash.new
-		puts('[I] getting games for: ' + user.summary.profile['personaname'])
-		user.owned_games(include_played_free_games: true, include_appinfo: true).games.each { |game|
-			@owned[game['appid']] = game['name']
-		}
-		return @owned
-	end
+    # found a way to get from steamid to display names (the gem's methods were odd/slow)
+
+    # take in the users community id and convert the friend list
+    # from IDs to usernames as seen by the user normally
+    # returns: array of user personas as displayed in friends list
+    def getFriendPersonas(apikey, id, steamIdKeys)
+        @personas = []
+        response = $conn.get 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + apikey + '&steamids=' + steamIdKeys
+        JSON.parse(response.body)['response']['players'].each { |friend|
+            @personas.push(friend['personaname'])
+        }
+        return @personas
+    end
 
 
-	#take in the app IDs and return a hash of {id => name}...
-	def getGameNames(idArr)
-		puts(idArr)
-		# TODO -- getAllOwned should return appid and name to refactor this
-
-	end
-
-
-	# get the same games a user has
-	# with the original user
-	# returns: array of games both users have
-	def getSharedGames(firID, secID)###varArr)
-		@same = []
-		firP = getUser(firID)
-		puts('[I] getting shared games for: ' + firID.to_s + ' and friends')
-		# # TODO ?? -- take array of user IDs and make hash of ID => user info
-		# @users = {}
-		# varArr.each{ |userID|
-		# 	@usr = getUser(userID)
-		# 	@users[userID.to_s] = @usr
-		# }
-		secP = getUser(secID)
-		mainGames = getAllOwned(firP)
-		newPgames = getAllOwned(secP)
-		puts('[I] new size: ' + newPgames.size.to_s)
-		puts('[I] old size: ' + mainGames.size.to_s)
-		@same = compareGames(mainGames, newPgames)
-		return @same
-	end
+    # hash of owned game data with array of games
+    # separate out the app IDs of games owned for usage
+    # returns: array of games user owns/f2p played
+    def getAllOwned(user)
+        @owned = Hash.new
+        puts('[I] getting games for: ' + user.summary.profile['personaname'])
+        user.owned_games(include_played_free_games: true, include_appinfo: true).games.each { |game|
+            @owned[game['appid']] = game['name']
+        }
+        return @owned
+    end
 
 
-	# need to find the ruby-esque way of doing this
-	# emun.detect or enum.find_all/find_index?
-	# grep/grep_v?
-	def compareGames(oneHash, twoHash)
-		firstArr = oneHash.keys
-		secArr = twoHash.keys
-		@same = Hash.new
-		puts('[I] comparing games...')
-		if (firstArr.size < secArr.size)
-			puts('[I] first < second')
-			firstArr.each { |first|
-				secArr.each { |second|
-					if (second == first)
-						@same[second] = twoHash[second]
-						break # break - found the game
-					end
-		}}
-		else # only search the smaller of the two
-			puts('[I] second < first')
-			secArr.each { |first|
-				firstArr.each { |second|
-					if (second == first)
-						@same[second] = twoHash[second]
-						break # break - found the game
-					end
-			}}
-		end
-		puts('[I] nSame: ' + @same.size.to_s)
-		return @same
-	end
+    # get the same games a user has
+    # with the original user
+    # returns: array of games both users have
+    def getSharedGames(firID, secID)###varArr)
+        @same = []
+        firP = getUser(firID)
+        puts('[I] getting shared games for: ' + firID.to_s + ' and friends')
+        # # TODO ?? -- take array of user IDs and make hash of ID => user info
+        # @users = {}
+        # varArr.each{ |userID|
+        #   @usr = getUser(userID)
+        #   @users[userID.to_s] = @usr
+        # }
+        secP = getUser(secID)
+        mainGames = getAllOwned(firP)
+        newPgames = getAllOwned(secP)
+        puts('[I] new size: ' + newPgames.size.to_s)
+        puts('[I] old size: ' + mainGames.size.to_s)
+        @same = compareGames(mainGames, newPgames)
+        return @same
+    end
 
 
-	# take in a couple users and desired games
-	# returns: hash of achievements have in common for games
-	def getSharedAchievements(usrOne, usrTwo, gameArr)
-		puts('[I] getting shared achievements for users:')
-		puts('    and games:')
-	end
+    # need to find the ruby-esque way of doing this
+    # emun.detect or enum.find_all/find_index?
+    # grep/grep_v?
+    def compareGames(oneHash, twoHash)
+        firstArr = oneHash.keys
+        secArr = twoHash.keys
+        @same = Hash.new
+        puts('[I] comparing games...')
+        if (firstArr.size < secArr.size)
+            puts('[I] first < second')
+            firstArr.each { |first|
+                secArr.each { |second|
+                    if (second == first)
+                        @same[second] = twoHash[second]
+                        break # break - found the game
+                    end
+            }}
+        else # only search the smaller of the two
+            puts('[I] second < first')
+            secArr.each { |first|
+                firstArr.each { |second|
+                    if (second == first)
+                        @same[second] = twoHash[second]
+                        break # break - found the game
+                    end
+            }}
+        end
+        puts('[I] nSame: ' + @same.size.to_s)
+        return @same
+    end
 
 
-	# The steam API method for player summaries is dynamic
-	# https://stackoverflow.com/questions/14958981/ruby-convert-array-into-function-arguments
-	# take in array of friend IDs, return hash of all friend data
-	def getAllFriendData(usr)
-		puts('[I] retrieving friend data for: ' + usr.summary.profile['personaname'])
-		friendArr = getFriendIDstr(usr).split(',')
-		@data = SteamWebApi::Player.summary(friendArr)
-		if (@data.success == true)
-			puts('[I] success')
-			return @data.players
-		end
-		puts('[!!] Failed getting all friend data')
-		puts()
-		puts(@data)
-		puts()
-		return Hash.new
-	end
+    # take in a couple users and desired games
+    # returns: hash of achievements have in common for games
+    def getSharedAchievements(usrOne, usrTwo, gameArr)
+        puts('[I] getting shared achievements for users:')
+        puts('    and games:')
+    end
+
+
+    # The steam API method for player summaries is dynamic
+    # take in array of friend IDs, return hash of all friend data
+    def getAllFriendData(usr)
+        puts('[I] retrieving friend data for: ' + usr.summary.profile['personaname'])
+        friendArr = getFriendIDstr(usr).split(',')
+        @data = SteamWebApi::Player.summary(friendArr)
+        if (@data.success == true)
+            puts('[I] success')
+            return @data.players
+        end
+        puts('[!!] Failed getting all friend data')
+        puts()
+        puts(@data)
+        puts()
+        return Hash.new
+    end
+
+
+    # take in game Hash and determine if game supports mp
+    def getGameMultiplayerInfo(conn, gameHash)
+        # http://store.steampowered.com/api/appdetails?appids=GAMEHASH.keys[index]
+        # json[appid][data][categories][0][id] # 2, singleplayer
+        # json[appid][data][categories][1][id] # 1, multiplayer
+        # json[appid][data][categories][4][id] # 4, cross-platform mp
+    end
 
 
 end
+
+=begin
+        SAMPLE JSON RESPONSE
+        {
+            "113020":{
+                "success":true,
+                "data":{
+                        ...
+                    "categories":[
+                        {
+                           "id":2,
+                           "description":"Single-player"
+                        },{
+                           "id":1,
+                           "description":"Multi-player"
+                        },{
+                           "id":9,
+                           "description":"Co-op"
+                        },{
+                           "id":24,
+                           "description":"Shared\/Split Screen"
+                        },{
+                           "id":27,
+                           "description":"Cross-Platform Multiplayer"
+                        },{
+                           "id":22,
+                           "description":"Steam Achievements"
+                        },{
+                           "id":28,
+                           "description":"Full controller support"
+                        },{
+                           "id":29,
+                           "description":"Steam Trading Cards"
+                        },{
+                           "id":30,
+                           "description":"Steam Workshop"
+                        },{
+                           "id":23,
+                           "description":"Steam Cloud"
+                        },{
+                           "id":25,
+                           "description":"Steam Leaderboards"
+                        },{
+                           "id":17,
+                           "description":"Includes level editor"
+                        }
+                    ],
+                    "platforms":{  
+                        "windows":true,
+                        "mac":true,
+                        "linux":true
+                    },
+                    ...
+                }
+         }
+
+=end
